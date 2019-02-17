859f7f24b2bed71b4c3f6337a11645b51e332e11
Chenyu Zhang <chenyu.heidi.z@gmail.com>
2019-02-16 20:51:53 -0500

    8:50

--- app/src/main/java/com/jhu/chenyuzhang/classroomhelper/PictureToNotesActivity.java
+++ app/src/main/java/com/jhu/chenyuzhang/classroomhelper/PictureToNotesActivity.java
@@ -1,13 +1,58 @@
 package com.jhu.chenyuzhang.classroomhelper;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 public class PictureToNotesActivity extends AppCompatActivity {
 
+    EditText display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_to_notes);
+
+        display = findViewById(R.id.display);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+        if(!textRecognizer.isOperational()) {
+            Toast.makeText(getApplicationContext(),"Could not get the Text", Toast.LENGTH_SHORT).show();
+        } else {
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+
+            SparseArray<TextBlock> items = textRecognizer.detect(frame);
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int i=0; i<items.size(); ++i) {
+                TextBlock myItem = items.valueAt(i);
+                sb.append(myItem.getValue());
+                sb.append("\n");
+            }
+
+            display.setEnabled(true);
+            display.setText(sb.toString());
+            display.setEnabled(false);
+
+        }
     }
 }
